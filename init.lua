f3c8d866f78cf1e0b4c9299f1f49f45a0e547b8b
lingege32 <lingege32@gmail.com>
2022-08-18 06:22:42 +0000

    add lspconfig to init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,17 @@
 vim.cmd('source ~/.vimrc')
 
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Only required if you have packer configured as `opt`
+--return require('packer').startup(function(use)
+	--local use = require('packer').use
+	--require('packer').startup(function()
+		--use 'wbthomason/packer.nvim' -- Package manager
+		--use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+	--end)
+    --require('lspconfig')['clangd'].setup{
+        --cmd = {"/remote/vgrnd106/chielin/bin/clangd_util/zebuClangd"},
+    --}
 
---local use = require('packer').use
---require('packer').startup(function()
-  --use 'wbthomason/packer.nvim' -- Package manager
-  --use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
 --end)
 
----- Lsp configuration URL: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
---require('lspconfig')['clangd'].setup{
-    --cmd = {"/remote/vgrnd106/chielin/bin/clangd_util/zebuClangd"},
-    --filetypes = {"c", "cpp", "objc", "objcpp", "cuda" }
---}
+
