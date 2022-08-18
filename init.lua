aade2487a4a3153adebaa5ed28b22248b8ca7ffc
lingege32 <lingege32@gmail.com>
2022-08-17 22:53:24 -0700

    add init.lua

---
+++ init.lua
@@ -0,0 +1,14 @@
+vim.cmd('source /remote/us01home50/chielin/.vimrc')
+
+
+--local use = require('packer').use
+--require('packer').startup(function()
+  --use 'wbthomason/packer.nvim' -- Package manager
+  --use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+--end)
+
+---- Lsp configuration URL: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+--require('lspconfig')['clangd'].setup{
+    --cmd = {"/remote/vgrnd106/chielin/bin/clangd_util/zebuClangd"},
+    --filetypes = {"c", "cpp", "objc", "objcpp", "cuda" }
+--}
